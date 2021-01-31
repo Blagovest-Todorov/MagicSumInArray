3c3a045a696a51e02e4433f60fecaa454735f76a
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-01-31 15:35:09 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace PrintArrayPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < arrNumbers.Length; i++)
+            {
+                int currNumber = arrNumbers[i];
+
+                for (int j = i+1; j < arrNumbers.Length; j++)
+                {
+                    int sumPairs = currNumber + arrNumbers[j];
+
+                    if (sumPairs == inputNumber)
+                    {
+                        Console.WriteLine(currNumber + " " + arrNumbers[j]);
+                    }
+
+                }
+
+                
+
+
+            }
+        }
+    }
+}
